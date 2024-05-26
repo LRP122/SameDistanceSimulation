5e5924995fae319bdb4c3736a19ab14420a819de
Luis Reitmeier <reitmeierluis@icloud.com>
2024-05-26 14:15:39 +0200

    New files and fun stuff

--- python-code/gentools.py
+++ python-code/gentools.py
@@ -5,25 +5,29 @@ import pandas as pd
 import numpy as np
 
 
-Number_Points = 10
+
 class Points():
 
     def __init__(self):
         self.x = round(random.uniform(0,100),1)
         self.y = round(random.uniform(0,100),1)
 
-    def distance(self,x1,y1,x2,y2):
-        dist = (math.sqrt(pow((x1-x2),2) + pow((y1-y2)),2))
+    def distance(self,point1,point2):
+        x1 = point1[0]
+        y1 = point1[1]
+        x2 = point2[0]
+        y2 = point2[1]
+        dist = (math.sqrt(pow((x1-x2),2) + pow((y1-y2),2)))
         return dist
 
-    def create(self):
+    def create(self, Number_Points):
         self.Points_Created = [Points() for _ in range(Number_Points)]
         self.Points_Created_x = [P.x for P in self.Points_Created]
         self.Points_Created_y = [P.y for P in self.Points_Created]
         self.Points_Dicts = {}
         for count,x,y in zip([i for i in range(len(self.Points_Created_x))],self.Points_Created_x,self.Points_Created_y):
-            self.Points_Dicts[count] = {1 : x,
-                                        2 : y}
+            self.Points_Dicts[count] = {0 : x,
+                                        1 : y}
         return self.Points_Dicts
 
     def sketch(self,slope,intercept):
@@ -38,17 +42,21 @@ class Points():
         plt.axis('equal')
         return
 
-    def find_partners(self):
+    def find_partners(self,Number_Points):
         self.big_partner_dict = {}
         self.small_partner_dict = {}
         self.small_partner_dict_array = []
         for i in range(Number_Points):
-            for b in range(1, 3):
-                self.small_partner_dict[b] = random.randint(1, 9)
+            for b in range(0, 2):
+                self.small_partner_dict[b] = random.randint(0, Number_Points-1)
+                while self.small_partner_dict[b] == i:
+                    self.small_partner_dict[b] = random.randint(0, Number_Points - 1)
+                while b != 0 and self.small_partner_dict[b] == self.small_partner_dict[b-1]:
+                    self.small_partner_dict[b] = random.randint(0, Number_Points - 1)
             self.small_partner_dict_array.append(self.small_partner_dict)
             self.small_partner_dict = {}
 
-        Keys = range(1, (Number_Points))
+        Keys = range(0, (Number_Points))
 
         for i in Keys:
             self.big_partner_dict[i] = self.small_partner_dict_array[i]
@@ -60,10 +68,10 @@ class Points():
         for any point on this line the condition of same distance will be met
         y = mx + b
         """
-        x1 = point1[1]
-        y1 = point1[2]
-        x2 = point2[1]
-        y2 = point2[2]
+        x1 = point1[0]
+        y1 = point1[1]
+        x2 = point2[0]
+        y2 = point2[1]
         mid_x = ((x1 + x2)/2)
         mid_y = ((y1 + y2) /2)
 
@@ -72,19 +80,41 @@ class Points():
         elif x2 > x1:
             m = ((y2 - y1) / (x2 - x1))
             m_perpendicular = (-1/m)
-            print(f"m ist {m} \nmp ist {m_perpendicular}")
             b = y1 - x1 * m
             b_perpendicular = mid_y - mid_x * m_perpendicular
         elif x1 > x2:
             m = ((y1 - y2) / (x1 - x2))
             m_perpendicular = (-1/m)
-            print(f"m ist {m} \nmp ist {m_perpendicular}")
             b = y1 - x1 * m
             b_perpendicular = mid_y - mid_x * m_perpendicular
-        return round(m, 4), round(b, 4), round(m_perpendicular, 4), round(b_perpendicular, 4)
+        return round(m_perpendicular, 4), round(b_perpendicular, 4), mid_x
 
-    def on_the_right_track(self, b, m, points, partnedict, n_point):
-        Point = points[n_point]
+    def new_position(self,m_perpendicular, b_perpendicular, mid_x, point):
+        x_offset = random.uniform(-10,10)
+        new_x = point[1] + x_offset
+        new_y = m_perpendicular * new_x + b_perpendicular
+        point[0] = new_x
+        point[1] = new_y
+        return point
+
+    def plot_linear_line(self,slope, intercept):
+         # Generate a range of x values
+         x = np.linspace(-0, 100, 400)
+         y = slope * x + intercept
+         plt.plot(x, y, '-r')
+         plt.title('Linear Line Plot')
+         plt.xlabel('x')
+         plt.ylabel('y')
+
+    def minimizer(self,point1,point2,point3):
+        DistAB = self.distance(point1,point2)
+        DistAC = self.distance(point1,point3)
+        DistCB = self.distance(point2,point3)
+
+        return DistAC + DistCB + DistCB
+
+
+
 
 
 
--- python-code/simulation.py
+++ python-code/simulation.py
@@ -1,30 +1,51 @@
 from gentools import *
 import matplotlib.pyplot as plt
+import itertools
+
+plt.ion()
+a= 0
 
 # create an instance of the Points class
+Number_Points = 3
+P = Points()
+points = P.create(Number_Points)
+partners = P.find_partners(Number_Points)
+#for i in range(Number_Points):
+ #   plt.scatter(points[i][0],points[i][1])
+#plt.show()
+combinations = list(itertools.combinations(points,2))
+print(partners)
+for i in range(10):
+    for i in range(Number_Points):
+        m_perpendicular, b_perpendicular, x_mid = (P.find_line_between_partners(
+                points[partners[i][0]],
+                points[partners[i][1]]))
+        if abs(P.distance(points[i],points[partners[i][0]]) - P.distance(points[i],points[partners[i][1]])) > 5:
+            while a < 100:
+                m_perpendicular, b_perpendicular, x_mid = (P.find_line_between_partners(
+                    points[partners[i][0]],
+                    points[partners[i][1]]))
+                test_point = P.new_position(m_perpendicular,b_perpendicular,x_mid, points[i])
+                DistanceZero = float("inf")
+                DistanceOne = P.minimizer(test_point, points[partners[i][0]],points[partners[i][1]])
+                if DistanceOne < DistanceZero:
+                    points[i] = test_point
+                a += 1
+        else:
+            print("Nah genug")
+        plt.clf()
+        for k in range(Number_Points):
+            plt.scatter(points[k][0], points[k][1])
+        plt.draw()
+        plt.pause(0.1)
 
-points_instance = Points()
 
+#for i in range(Number_Points):
+ #   plt.scatter(points[i][0],points[i][1])
+P.plot_linear_line(m_perpendicular,b_perpendicular)
+#plt.show()
 
-# call the create_and_sketch method on the instance
-K = points_instance.create()
-partners = points_instance.find_partners()
-#print(K)
-
-#print(partners)
-first_partner = K[1]
-second_partner = K[2]
-print(first_partner)
-print(second_partner)
-
-m, b, mp, bp = points_instance.find_line_between_partners(first_partner,second_partner)
-
-points_instance.sketch(m,b)
-points_instance.sketch(mp,bp)
-print(f"m ist {m} \nmp ist {mp}")
-print(f"m ist {b} \nmp ist {bp}")
-plt.scatter([first_partner[1], second_partner[1]],[first_partner[2], second_partner[2]], color = "red" )
-plt.show()
-#print(K.find_partners())
-
+print(P.distance(points[0],points[1]),P.distance(points[2],points[1]))
+print(P.distance(points[0],points[2]),P.distance(points[0],points[1]))
+print(P.distance(points[1],points[2]),P.distance(points[0],points[2]))
 
